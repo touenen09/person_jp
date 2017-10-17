1f23739132d801e05ad4ed9e50f13039f66d5d4f
xczhang <xczhang@XCZHANG-CN.cn.oracle.com>
2017-10-17 19:36:16 +0800

    update

---
+++ src/main/java/com/mitsubishi/demo/functions/personjobhistory/form/PersonJobHistoryTableForm.java
@@ -0,0 +1,107 @@
+package com.mitsubishi.demo.functions.personjobhistory.form;
+
+import java.util.Date;
+
+public class PersonJobHistoryTableForm {
+
+	private String personNum;
+
+	private String appointment;
+
+	private Date effectiveDate;
+
+	private String effectiveDateString;
+
+	private String action;
+
+	private String actionReason;
+
+	private String department;
+
+	private String job;
+
+	private String location;
+
+	private String group1;
+
+	public String getPersonNum() {
+		return personNum;
+	}
+
+	public void setPersonNum(String personNum) {
+		this.personNum = personNum;
+	}
+
+	public String getAppointment() {
+		return appointment;
+	}
+
+	public void setAppointment(String appointment) {
+		this.appointment = appointment;
+	}
+
+	public Date getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public void setEffectiveDate(Date effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+
+	public String getEffectiveDateString() {
+		return effectiveDateString;
+	}
+
+	public void setEffectiveDateString(String effectiveDateString) {
+		this.effectiveDateString = effectiveDateString;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public String getActionReason() {
+		return actionReason;
+	}
+
+	public void setActionReason(String actionReason) {
+		this.actionReason = actionReason;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getGroup1() {
+		return group1;
+	}
+
+	public void setGroup1(String group1) {
+		this.group1 = group1;
+	}
+
+}
