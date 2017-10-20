3adf1443c35397a4b140b8c8ca057c5bbb0cf71c
xczhang <xczhang@XCZHANG-CN.cn.oracle.com>
2017-10-20 12:50:37 +0800

    update

--- src/main/java/com/mitsubishi/demo/common/config/AppConfig.java
+++ src/main/java/com/mitsubishi/demo/common/config/AppConfig.java
@@ -9,8 +9,8 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -19,12 +19,11 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import com.mitsubishi.demo.common.mapper.InformationTableMapper;
 import com.mitsubishi.demo.common.mapper.PersonCareerDataMapper;
 import com.mitsubishi.demo.common.mapper.PersonJobHistoryTableMapper;
 import com.mitsubishi.demo.common.mapper.PersonTableMapper;
-
-import net.sf.log4jdbc.Log4jdbcProxyDataSource;
 
 @Configuration
 public class AppConfig {
@@ -33,28 +32,22 @@ public class AppConfig {
 	DataSourceProperties dataSourceProperties;
 	DataSource dataSource;
 
-	// @Bean(destroyMethod = "close")
-	@Bean
-	DataSource realDataSource() {
-		DataSourceBuilder factory = DataSourceBuilder.create(this.dataSourceProperties.getClassLoader())
-				.url(this.dataSourceProperties.getUrl()).username(this.dataSourceProperties.getUsername())
-				.password(this.dataSourceProperties.getPassword());
-		this.dataSource = factory.build();
-		// return this.dataSource;
-		return new Log4jdbcProxyDataSource(this.dataSource);
-	}
-
-	// // @Bean(destroyMethod = "close")
-	// @ConfigurationProperties("spring.datasource.druid")
-	// public DataSource dataSource() {
-	// return DruidDataSourceBuilder.create().build();
-	//
-	// }
-	//
 	// @Bean
-	// public DataSource realDataSource() {
-	// return new Log4jdbcProxyDataSource(dataSource());
+	// DataSource realDataSource() {
+	// DataSourceBuilder factory =
+	// DataSourceBuilder.create(this.dataSourceProperties.getClassLoader())
+	// .url(this.dataSourceProperties.getUrl()).username(this.dataSourceProperties.getUsername())
+	// .password(this.dataSourceProperties.getPassword());
+	// this.dataSource = factory.build();
+	// // return this.dataSource;
+	// return new Log4jdbcProxyDataSource(this.dataSource);
 	// }
+
+	@Bean(destroyMethod = "close")
+	@ConfigurationProperties("spring.datasource.druid")
+	public DataSource realDataSource() {
+		return DruidDataSourceBuilder.create().build();
+	}
 
 	@Bean
 	public SqlSessionFactory getSqlSessionFactory() {
